38282d062b48eb1f20ead3b667d32bb8d2317262
rrathbone <rachellerathbone@gmail.com>
2016-09-19 15:51:04 -0700

    Checking against test and fixing error

--- pets.js
+++ pets.js
@@ -7,40 +7,54 @@ const petsPath = path.join(__dirname, 'pets.json');
 const node = path.basename(process.argv[0]);
 const file = path.basename(process.argv[1]);
 const cmd = process.argv[2];
+const age = Number(process.argv[3]);
+const kind = process.argv[4];
+const name = process.argv[5];
 
 if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', (err, data) => {
     if (err) throw err;
 
     const pets = JSON.parse(data);
-
-    console.log(pets);
-  });
-} else if (cmd === 'create') {
-  fs.readFile(petsPath, 'utf8', (readErr, data) => {
-    if (readErr) throw readErr;
-
-    const pets = JSON.parse(data);
-    const pet = process.argv[3];
-
-    if (!pet) {
-      console.error(`Usage: ${node} ${file} ${cmd} pet_NAME`);
+    if (age >= pets.length || age < 0) {
+      console.error(`Usage: ${node} ${file} read INDEX`);
       process.exit(1);
+    } else if (!age) {
+      console.log(pets);
+    } else {
+      console.log(pets[age]);
     }
 
-    pets.push(pet);
+  });
+  } else if (cmd === 'create') {
+    fs.readFile(petsPath, 'utf8', (readErr, data) => {
+      if (readErr) throw readErr;
 
-    const petsJSON = JSON.stringify(pets);
+      const pets = JSON.parse(data);
 
-      fs.writeFile(petsPath, petsJSON, (writeErr) => {
-        if (writeErr) {
-          throw writeErr;
-        }
+      if (!age || !kind || !name || isNaN(age)) {
+        console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+        process.exit(1);
+      }
 
-        console.log(pet);
+      let newPet = {}
+      newPet.age = age;
+      newPet.kind = kind;
+      newPet.name = name;
+      pets.push(newPet);
+
+
+      const petsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, petsJSON, (writeErr) => {
+          if (writeErr) {
+            throw writeErr;
+          }
+
+          console.log(newPet);
+        });
       });
-    });
-  } else {
-    console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
-    process.exit(1);
-  }
+    } else {
+      console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+      process.exit(1);
+    }
