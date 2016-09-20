33b017b9878c5b677d67b13c977fa0f2d04b40a4
rrathbone <rachellerathbone@gmail.com>
2016-09-19 17:24:16 -0700

    Complete first bonus

--- pets.js
+++ pets.js
@@ -7,54 +7,67 @@ const petsPath = path.join(__dirname, 'pets.json');
 const node = path.basename(process.argv[0]);
 const file = path.basename(process.argv[1]);
 const cmd = process.argv[2];
-const age = Number(process.argv[3]);
-const kind = process.argv[4];
-const name = process.argv[5];
+const fourth = process.argv[3];
+const fifth = process.argv[4];
+const sixth = process.argv[5];
+const seventh = process.argv[6];
 
 if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', (err, data) => {
     if (err) throw err;
 
     const pets = JSON.parse(data);
-    if (age >= pets.length || age < 0) {
+    if (Number.isNaN(fourth)) {
+      console.log(pets);
+      process.exit();
+    }
+
+    if (fourth >= pets.length || fourth < 0){
       console.error(`Usage: ${node} ${file} read INDEX`);
       process.exit(1);
-    } else if (!age) {
+    }
+    else if (!fourth){
       console.log(pets);
     } else {
-      console.log(pets[age]);
+      console.log(pets[fourth]);
     }
-
   });
-  } else if (cmd === 'create') {
+} else if (cmd === 'create' || cmd === 'update') {
     fs.readFile(petsPath, 'utf8', (readErr, data) => {
-      if (readErr) throw readErr;
+    if (readErr) throw readErr;
 
-      const pets = JSON.parse(data);
+    const pets = JSON.parse(data);
 
-      if (!age || !kind || !name || isNaN(age)) {
-        console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
-        process.exit(1);
-      }
-
-      let newPet = {}
-      newPet.age = age;
-      newPet.kind = kind;
-      newPet.name = name;
-      pets.push(newPet);
-
-
-      const petsJSON = JSON.stringify(pets);
-
-        fs.writeFile(petsPath, petsJSON, (writeErr) => {
-          if (writeErr) {
-            throw writeErr;
-          }
-
-          console.log(newPet);
-        });
-      });
-    } else {
-      console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+    if (!fourth || !fifth || !sixth || isNaN(fourth) || !seventh) {
+    if (cmd === 'create'){
+      console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+    } else if (cmd === 'update'){
+      console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`);
+    }
       process.exit(1);
     }
+
+    if (cmd === 'create'){
+      var newPet = {};
+        newPet.age = Number(fourth);
+        newPet.kind = fifth;
+        newPet.name = sixth;
+        pets.push(newPet);
+    } else if (cmd === 'update'){
+      var newPet = {};
+      newPet.age = Number(fifth);
+      newPet.kind = sixth;
+      newPet.name = seventh;
+      pets[Number(fourth)] = newPet;
+    }
+
+    const petsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
+      if (writeErr) throw writeErr;
+    });console.log(newPet);
+  });
+} else {
+    console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+    process.exit(1);
+}
