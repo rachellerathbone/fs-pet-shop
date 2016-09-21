693d184bdc9383974ca7165fbac31dc20dc572ca
rrathbone <rachellerathbone@gmail.com>
2016-09-21 15:21:34 -0700

    create expressServer.js

---
+++ expressServer.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const guestsPath = path.join(__dirname, 'guests.json');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.disable('x-powered-by');
+app.use(morgan('short'));
+app.use(bodyParser.json());
+
+app.get('/guests', (req, res) => {
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    const guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  });
+});
+
+app.post('/guests', (req, res) => {
+  fs.readFile(guestsPath, 'utf8', (readErr, guestsJSON) => {
+    if (readErr) {
+      console.error(readErr.stack);
+      return res.sendStatus(500);
+    }
+
+    const guests = JSON.parse(guestsJSON);
+    const guest = req.body.name;
+
+    if (!guest) {
+      return res.sendStatus(400);
+    }
+
+    guests.push(guest);
+
+    const newGuestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, newGuestsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'text/plain');
+      res.send(guest);
+    });
+  });
+});
+
+app.get('/guests/:id', (req, res) => {
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    const id = Number.parseInt(req.params.id);
+    const guests = JSON.parse(guestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
